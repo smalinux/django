660c3b9c5f732c2aece4fba6a45d9a25f97cfcb8
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2022-03-27 17:43:15 -0400

    Full example: get data from db and show it in it's templates...
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Profile
+#from .models import
 
 # Create your views here.
+def profile(request):
+    return render(request, 'profile.html')
+
+def profiles(request):
+    return render(request, 'profiles.html', {'pro': Profile.objects.all()})
