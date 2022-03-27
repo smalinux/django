dae8f232d760c5867cc2ebf690100a46d9839fe7
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2022-03-27 10:19:17 -0400

    Create minimal template
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('index page')
+    #return HttpResponse('index page')
+    return render(request, 'pages/index.html')
 
 def about(request):
     return HttpResponse('about pages')
