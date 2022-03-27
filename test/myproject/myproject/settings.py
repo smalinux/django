0c0c97462d4e87fc28bc91d320994f973c2d52f5
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2022-03-26 22:44:01 -0400

    `docs`: create new pages url
    App(view.py | urls.py) <-> myproject(urls.py)
    
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- test/myproject/myproject/settings.py
+++ test/myproject/myproject/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
+    'groups.apps.GroupsConfig',
+    'profiles.apps.ProfilesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
