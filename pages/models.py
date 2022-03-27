642d7e2c7a4d175a63e4b36df3e046b9b77948e7
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2022-03-27 14:18:09 -0400

    models: add table to db
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    content = models.TextField()
+    image = models.ImageField(upload_to='photos/%y/%m/%d')
+    active = models.BooleanField(default=True)
