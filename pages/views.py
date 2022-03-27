349a01267b55b0642c477ec23203d714c7c45b61
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2022-03-27 11:23:53 -0400

    filters
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+import os
 #from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     #return HttpResponse('index page')
-    return render(request, 'pages/index.html')
+    x = {
+            'name':'ahmed',
+            'last':'',
+            'full':'Sohaib Mohamed Abdel fattah',
+            'age': 62783467324,
+        }
+    return render(request, 'pages/index.html', x)
 
 def about(request):
     return HttpResponse('about pages')
